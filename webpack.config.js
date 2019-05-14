e20a365339e05207a7e61e0a06a29638ae9753ef
NavinNavi19 <navin007.a@gmail.com>
2019-05-14 08:16:48 +0000

    Fixed Path Error

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    filename: "webpack.js",
-    path: path.resolve(__dirname + "code")
+    filename: "hello.js",
+    path: path.resolve(__dirname + "/dist")
   }
 };
