e49012ec8edb5a958b40831af0ff0801e55e4f62
NavinNavi19 <navin007.a@gmail.com>
2019-05-15 05:42:15 +0000

    Added HTML Loader Plugin to support ContentHashing

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -6,17 +7,19 @@ module.exports = {
   // devtool: "none",
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
+    filename: "main.[contentHash].js",
     path: path.resolve(__dirname + "/dist")
   },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/template.html" })],
   module: {
     rules: [
       {
-        test: /\.css$/,
+        test: /\.scss$/,
+        //SCSS Loader loads the SCSS into CSS.
         //CSS Loader loads the CSS and converts them into JS.
         //Style Loader loads that CSS JS into the DOM.
-        //Should be arranged in a reverse order
-        use: ["style-loader", "css-loader"]
+        //Should be arranged in a reverse order.
+        use: ["style-loader", "css-loader", "sass-loader"]
       }
     ]
   }
